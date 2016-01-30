540c1931144cd2a717d19340016bf5625d1fc0ef
sarvin <scoachbuilder@dealersocket.com>
2016-01-30 01:10:25 -0600

    parse npr json and display on show

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,9 +1,18 @@
 class ChannelsController < ApplicationController
+  require 'open-uri'
+
   def index
     @channels= Channel.all
   end
 
   def show
     @channel = Channel.find(params[:id])
+    @hash = get_latest(@channel)
+  end
+
+  def get_latest(channel)
+    url = "http://api.npr.org/query?id=98679384&fields=show,audio,multimedia,titles,song,album,text&apiKey=#{Rails.configuration.x.api_key}&output=json"
+    buffer = open(url).read
+    hash = JSON.parse(buffer)
   end
 end
